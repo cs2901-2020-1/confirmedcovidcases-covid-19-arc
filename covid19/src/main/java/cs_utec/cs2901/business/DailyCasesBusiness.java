403e38f12a1061a5803acfd4038a277468f12b97
luisejv <luisejv@LAPTOP-J7CR1JEP.localdomain>
2020-04-03 08:26:30 -0500

    DailyCases Business added

--- covid19/src/main/java/cs_utec/cs2901/business/DailyCasesBusiness.java
+++ covid19/src/main/java/cs_utec/cs2901/business/DailyCasesBusiness.java
@@ -1,9 +1,13 @@
 package cs_utec.cs2901.business;
 
+import cs_utec.cs2901.data.DailyCases;
 import cs_utec.cs2901.repositories.DailyCasesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -11,4 +15,45 @@ public class DailyCasesBusiness {
     @Autowired
     private DailyCasesRepository repository;
 
+    /**
+     * Create: Guarda un nuevo dailycase
+     * @param newDailyCase
+     * @return DailyCase guardado
+     */
+    public DailyCases create(DailyCases newDailyCase){
+        return repository.save(newDailyCase);
+    }
+
+    /**
+     * Obtiene todos los daily cases
+     * @return dailycases
+     */
+    public List<DailyCases> findAll(){
+        List<DailyCases> items = new ArrayList<>();
+        for (DailyCases item: repository.findAll()){
+            items.add(item);
+        }
+        return items;
+    }
+
+    /**
+     * Update: Actualiza un nuevo dailycase existente
+     * @param item
+     * @return Dailycase actualizado
+     */
+    public DailyCases update(DailyCases item){
+        DailyCases a = repository.findById(item.getId()).get();
+        if (a != null){
+            return repository.save(item);
+        }
+        return null;
+    }
+
+    /**
+     * Delete: Elimina un dailycase existente
+     * @param id
+     */
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
