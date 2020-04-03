42239d2c8125b3389c12298aabb8f0ecc9e4ac3b
luisejv <ljauregui@pucp.pe>
2020-04-03 07:52:37 -0500

    DailyCasesRepository added

--- covid19/src/main/java/cs_utec/cs2901/repositories/DailyCasesRepository.java
+++ covid19/src/main/java/cs_utec/cs2901/repositories/DailyCasesRepository.java
@@ -1,4 +1,17 @@
 package cs_utec.cs2901.repositories;
 
-public class DailyCasesRepository {
+
+import cs_utec.cs2901.data.DailyCases;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Manejo de persistencia de la entidad DailyCases, soporta las operaciones básicas CRUD:
+ * - CREATE: INSERT INTO dailycases VALUES (id_values, cantidad, 'fecha');
+ * - READ ONE: SELECT * FROM dailycases WHERE id = 2;
+ * - READ ALL: SELECT * FROM dailycases;
+ * - UPDATE: UPDATE dailycases SET cantidad = 3 WHERE id = 5;
+ * - DELETE: DELETE FROM dailycases WHERE id = 3;
+ */
+
+public interface DailyCasesRepository extends CrudRepository<DailyCases, Long> {
 }
